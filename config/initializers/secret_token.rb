1de61bc92fddf3e33d35737d62536dd70499fa34
Jeremy Woertink <jeremywoertink@gmail.com>
2013-10-18 17:38:45 -0700

    rails rumble woot woot

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Datingscene::Application.config.secret_key_base = 'a467b1621a853f782ec3f14790bdb4fc773f60ace4f9fe756a5ce0c50569e73c1feef5a07fddcaacc25c358912a1eb98a0654a4482774214728ce739cfdca298'
