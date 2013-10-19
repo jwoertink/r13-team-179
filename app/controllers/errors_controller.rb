d6469952e2146215495def24b24979717afdfe77
Jeremy Woertink <jeremywoertink@gmail.com>
2013-10-19 10:09:51 -0700

    error pages work now

---
+++ app/controllers/errors_controller.rb
@@ -0,0 +1,16 @@
+class ErrorsController < ApplicationController
+  layout 'errors'
+  
+  def not_found
+    render status: 404
+  end
+  
+  def internal_server_error
+    render status: 500
+  end
+  
+  def unprocessable_entity
+    render status: 422
+  end
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Datingscene::Application.routes.draw do
   root 'site#index'
   get '/privacy-policy' => 'site#privacy', as: :privacy
   get '/terms-of-service' => 'site#terms', as: :terms
+  # Error Pages
+  get '/404', :to => 'errors#not_found'
+  get '/422', :to => 'errors#unprocessable_entity'
+  get '/500', :to => 'errors#internal_server_error'
 end
