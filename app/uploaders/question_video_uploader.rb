7d905b4dd4a3074d6c562bc5177c1ed0d17b3778
Jeremy Woertink <jeremywoertink@gmail.com>
2013-10-20 15:52:27 -0700

    readjust carrierwave

--- app/uploaders/question_video_uploader.rb
+++ app/uploaders/question_video_uploader.rb
@@ -1,14 +1,9 @@
 # encoding: utf-8
 class QuestionVideoUploader < CarrierWave::Uploader::Base
-
+  storage :file
+  
   def store_dir
     "#{Rails.root}/../../shared/#{model.class.to_s.underscore}/#{model.category}/"
   end
-
-  # Override the filename of the uploaded files:
-  # Avoid using model.id or version_name here, see uploader/store.rb for details.
-  # def filename
-  #   "something.jpg" if original_filename
-  # end
 
 end
