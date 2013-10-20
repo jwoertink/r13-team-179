6b8a75a8eaa0d8689fd8fbc92ecaf7de17b50bc3
Jeremy Woertink <jeremywoertink@gmail.com>
2013-10-19 20:00:43 -0700

    stupid

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,12 +1,15 @@
 class AdminController < ApplicationController
   layout 'admin'
   before_filter :authenticate
+  
+  USERNAME = ENV['ADMIN_LOGIN']
+  PASSWORD = ENV['ADMIN_PASSWORD']
     
   protected
   
   def authenticate
     authenticate_or_request_with_http_basic do |username, password|
-      username == ENV['ADMIN_LOGIN'] && password == ENV['ADMIN_PASSWORD']
+      username == USERNAME && password == PASSWORD
     end
   end
   
