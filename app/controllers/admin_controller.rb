6f5fdd9719b85aff7d070c9ae1fb4c62014cedbb
Jeremy Woertink <jeremywoertink@gmail.com>
2013-10-19 20:04:12 -0700

    logging ~_~

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -9,6 +9,10 @@ class AdminController < ApplicationController
   
   def authenticate
     authenticate_or_request_with_http_basic do |username, password|
+      puts "username is #{username}\n\n"
+      puts "USERNAME is #{USERNAME}\n\n"
+      puts "password is #{password}\n\n"
+      puts "PASSWOD is #{PASSWORD}\n\n"
       username == USERNAME && password == PASSWORD
     end
   end
