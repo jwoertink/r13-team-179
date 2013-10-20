61dd22639a8ae49de4bb2649666877644244ba2a
Jeremy Woertink <jeremywoertink@gmail.com>
2013-10-20 15:53:06 -0700

    readjust carrierwave 2

--- app/uploaders/profile_video_uploader.rb
+++ app/uploaders/profile_video_uploader.rb
@@ -4,8 +4,8 @@ class ProfileVideoUploader < CarrierWave::Uploader::Base
   aws_bucket ENV['S3_BUCKET_NAME']
   aws_acl :public_read
   asset_host "https://s3-us-west-2.amazonaws.com/datingscene"
-  aws_authenticated_url_expiration 60 * 60 * 24 * 365
-  aws_credentials {access_key_id: ENV['S3_ACCESS_KEY_ID'], secret_access_key: ENV['S3_SECRET_KEY']}
+  aws_authenticated_url_expiration (60 * 60 * 24 * 365)
+  aws_credentials access_key_id: ENV['S3_ACCESS_KEY_ID'], secret_access_key: ENV['S3_SECRET_KEY']
   
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
